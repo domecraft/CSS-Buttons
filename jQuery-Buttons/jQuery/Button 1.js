a2f4f290dd2f276a773a273ba90f78440ec16de9
Christopher Durr <christopherdurr1@gmail.com>
2014-08-05 20:46:40 -0700

    Update Button 1.js

--- jQuery-Buttons/jQuery/Button 1.js
+++ jQuery-Buttons/jQuery/Button 1.js
@@ -1,8 +1,23 @@
-	$(document).ready(function () {
-	    $("#blue").mouseenter(function () {
-	        $("#green").show("fast");
+$(document).ready(function () {
+    var index = 0
+	    $("#blue").click(function () {
+            index = index + 1
+            if (index % 2==0) {
+	        $("#green").show(100);
+            }
 	    });
-	    $('body').click(function (event) {
-	        if (event.target.id != 'blue') $("#green").hide("fast");
+	    $("#blue").click(function () {
+            if (index%2==1) {
+	       $("#green").hide(100);
+            }
 	    });
+    $("#green").mouseover(function () {
+        $("#green").animate({
+            opacity: 0.8,
+            height: "400px"
+        }, 200);
+    });
+    $("#green").mouseleave(function () {
+        $("#green").animate({opacity: 1}, 200);
+    });
 	});
